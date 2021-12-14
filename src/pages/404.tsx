51a751072c2391d41cb5ab5b3aefc829edb18b23
Delacannon <delacannon@gmail.com>
2021-12-14 16:57:39 +0100

    404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,10 +4,8 @@ export const NotFoundPage = () => (
   <Container>
     <Header>Error 404</Header>
     <Text>
-      La página a la que intentas acceder no existe. Puedes volver al{" "}
-      <LinkSpan to={"."}>inicio</LinkSpan>, probar de hacer{" "}
-      <LinkSpan to={"/login"}>login</LinkSpan> o bien visitar el{" "}
-      <LinkSpan to={"/listado"}>listado</LinkSpan> de usuarios.
+      La página a la que intentas acceder no existe.{" "}
+      <LinkSpan to={"."}>Volver</LinkSpan>
     </Text>
   </Container>
 );
